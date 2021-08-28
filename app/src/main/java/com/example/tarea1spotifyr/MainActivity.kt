5d37dd9d86cb39a0d806a8c2ab51fa3e6f543028
Oswaldo <oswaldo@estudiantec.cr>
2021-08-28 15:40:28 -0600

    ya reproduce musica!

--- app/src/main/java/com/example/tarea1spotifyr/MainActivity.kt
+++ app/src/main/java/com/example/tarea1spotifyr/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.tarea1spotifyr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.util.Log;
+
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
@@ -11,12 +15,10 @@ import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
 
-
-
 class MainActivity : AppCompatActivity() {
 
     private val clientId = "6ecb1909988649768f2839eb68296f3a"
-    private val redirectUri = "http://com.example.tarea1spotifyr/callback"
+    private val redirectUri = "https://com.example.tarea1spotifyr/callback"
     private var spotifyAppRemote: SpotifyAppRemote? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +29,9 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val connectionParams = ConnectionParams.Builder(clientId)
-                .setRedirectUri(redirectUri)
-                .showAuthView(true)
-                .build()
-
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
 
         SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
             override fun onConnected(appRemote: SpotifyAppRemote) {
@@ -48,11 +49,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun connected() {
-        spotifyAppRemote?.playerApi?.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
-        spotifyAppRemote?.playerApi?.subscribeToPlayerState()?.setEventCallback {
-            val track: Track = it.track
-            Log.d("MainActivity", track.name + " by " + track.artist.name)
+        spotifyAppRemote?.let {
+            // Play a playlist
+            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
+            it.playerApi.play(playlistURI)
+            // Subscribe to PlayerState
+            it.playerApi.subscribeToPlayerState().setEventCallback {
+                val track: Track = it.track
+                Log.d("MainActivity", track.name + " by " + track.artist.name)
+            }
         }
+
     }
 
     override fun onStop() {
@@ -60,5 +67,6 @@ class MainActivity : AppCompatActivity() {
         spotifyAppRemote?.let {
             SpotifyAppRemote.disconnect(it)
         }
+
     }
 }
